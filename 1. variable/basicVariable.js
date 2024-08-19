458214ce1ba3b8dd6aa320c825caaada15245be1
zafar imam <zafarimam7032@gmail.com>
2024-08-20 02:01:02 +0530

    data type

---
+++ 1. variable/basicVariable.js
@@ -0,0 +1,14 @@
+console.info("let learn java script");
+console.info("this is java script basic");
+const accountId = "1322";
+console.info(accountId);
+// accountId = "2342331";//this is constant we can not able to changes
+console.log(accountId);
+let emaild = "zafar@gmail.com";
+console.info(emaild);
+emaild = "imam@gmail.com"; //let we can make change because this is change and it is not having any issues in scope
+console.info(emaild);
+var password = "122123";
+console.info(password);
+password = "243434"; //var type also we can make change but it is not reccommaned becasye it is having scope issues
+console.info(password);
---
+++ 1. variable/dataType.js
@@ -0,0 +1,71 @@
+// Boolean
+console.info("======================boolean=============");
+let checkFlag = true;
+console.log(checkFlag);
+console.log(typeof checkFlag);
+checkFlag = false; // java script support boolean ('true','false')
+console.log(checkFlag);
+console.log(typeof checkFlag);
+
+// Number
+console.info("=================Number===================");
+let accountNumber = 12112223;
+console.log(accountNumber);
+console.log(typeof accountNumber);
+
+// String
+console.info("================String==========================");
+let language = "Hindi";
+console.info(language);
+console.info(typeof language);
+
+// default value
+console.info("================default value===================");
+//const contsValue;// const variable must be intialize
+let roundedValue;
+console.info(roundedValue);
+console.info(typeof roundedValue);
+let objectValue = null;
+console.info(objectValue);
+console.info(typeof objectValue);
+
+//convert String to number
+console.info("=========convert String to Number==============");
+let serialNumber = "121233";
+
+let convertedNumnber = Number(serialNumber);
+
+console.info(convertedNumnber);
+console.info(typeof convertedNumnber);
+console.info(serialNumber);
+console.info(typeof serialNumber);
+
+// convertion boolean
+console.log("============convertion boolean=================");
+let nam = "232323";
+
+let check = Boolean(nam);
+
+console.log(check);
+let nam1 = "";
+
+let check1 = Boolean(nam1);
+
+console.log(check1);
+let nam2 = "zafar";
+
+let check2 = Boolean(nam2);
+
+console.log(check2);
+
+let nam3 = 1;
+
+let check3 = Boolean(nam3);
+
+console.log(check3);
+
+let nam4 = 0;
+
+let check4 = Boolean(nam4);
+
+console.log(check4);

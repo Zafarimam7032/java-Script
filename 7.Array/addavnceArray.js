caef5889516b00ec1862b3d30a05239b79030530
Zafar imam <zafarimam7032@gmail.com>
2024-08-28 01:07:32 +0530

    array

--- 7.Array/addavnceArray.js
+++ 7.Array/addavnceArray.js
@@ -1,6 +1,13 @@
 let arr3 = [23, 2, 4, 2, 4, [34, 4, 3, 53, [343, 43, 54, 34]]];
 
-console.log(arr3.flat(Infinity));
-let firstName = "zafar";
-let lastName = "imam";
-console.log(Array.of(firstName, lastName));
+// console.log(arr3.flat(Infinity));
+// let firstName = "zafar";
+// let lastName = "imam";
+// console.log(Array.of(firstName, lastName));
+
+const cities = ["pune", "hyderabad", "patna"];
+// cities.forEach((ar) => console.log(ar));
+
+cities
+  .map((mps) => (mps.length > 5 ? mps : ""))
+  .forEach((mps) => console.log(mps));

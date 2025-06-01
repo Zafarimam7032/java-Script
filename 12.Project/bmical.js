fb9b785283e98ade11ed05403679d6b07ff9fb9a
zafar imam <zafarimam7032@gmail.com>
2025-06-01 10:36:09 +0530

    project fix

--- 12.Project/bmical.js
+++ 12.Project/bmical.js
@@ -4,7 +4,9 @@ form.addEventListener("submit", function (e) {
   e.preventDefault();
   const height = parseInt(document.getElementById("height").value);
   const weight = parseInt(document.getElementById("weight").value);
-  const result = document.querySelector("#result");
+  console.log(height + " " + weight);
+
+  const result = document.querySelector(".result");
   if (height == "" || isNaN(height)) {
     result.innerHTML = "please inter valid text";
   } else if (weight == "" || isNaN(weight)) {

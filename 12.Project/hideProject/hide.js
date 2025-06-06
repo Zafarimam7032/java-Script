fffcce8556317e58679e6febab6599967eb13b0f
Zafar imam <zafarimam7032@gmail.com>
2025-06-06 17:44:51 +0530

    project

--- 12.Project/hideProject/hide.js
+++ 12.Project/hideProject/hide.js
@@ -1,6 +1,8 @@
 const oldata = document.querySelector(".no-count");
 oldata.addEventListener("click", function (e) {
   e.preventDefault();
-  const target = e.target;
-  target.remove();
+  if (e.target.tagName === "IMG") {
+    const target = e.target;
+    target.remove();
+  }
 });
